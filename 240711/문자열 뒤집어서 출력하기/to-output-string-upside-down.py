33aac096941a8d58fe393191f74967416e5e38dd
minimimin <139435516+minimimin@users.noreply.github.com>
2024-07-11 14:13:46 +0900

    Title: 문자열 뒤집어서 출력하기, Time: 193ms, Memory: 25MB, Status: Passed - CodeTree

--- 240711/문자열 뒤집어서 출력하기/to-output-string-upside-down.py
+++ 240711/문자열 뒤집어서 출력하기/to-output-string-upside-down.py
@@ -1,5 +1,7 @@
 from collections import deque
 
-words = deque([input() for _ in range(4)])
+words = deque()
+for _ in range(4):
+    words.append(input())
 for sunseo in range(4):
     print(words.pop()[::-1])
